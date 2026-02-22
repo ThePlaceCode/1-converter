ae913e19634cd831848e45a4a85cd06aee478111
Roman Bodanov <theplace.workplace@gmail.com>
2026-02-22 17:40:53 +0300

    Сделал

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// Константы конвертации
+const (
+	USDToEUR = 0.93
+	USDToRUB = 90.5
+	EURToRUB = USDToRUB / USDToEUR
+)
 
 func main() {
-	// Константы конвертации
-	const (
-		USDToEUR = 0.93
-		USDToRUB = 90.5
-		EURToRUB = USDToRUB / USDToEUR
-	)
+	beginCurrency := inputBeginCurrency()
+	amountCurrency := inputAmountCurrency()
+	endCurrency := inputEndCurrency(beginCurrency)
+
+	sum := calculate(amountCurrency, beginCurrency, endCurrency)
+	fmt.Println(sum)
 }
 
 func userInput() {
@@ -17,6 +26,80 @@ func userInput() {
 
 }
 
-func calculate(number float64, beginCurrency string, endCurrency string) float64 {
-	return 0.00
+func calculate(amount float64, beginCurrency string, endCurrency string) float64 {
+	var sum float64
+
+	switch {
+	case beginCurrency == "USD" && endCurrency == "EUR":
+		sum = amount * USDToEUR
+	case beginCurrency == "USD" && endCurrency == "RUB":
+		sum = amount * USDToRUB
+	case beginCurrency == "EUR" && endCurrency == "RUB":
+		sum = amount * EURToRUB
+	}
+
+	return sum
+}
+
+func inputBeginCurrency() string {
+	var currency string
+
+	for {
+		fmt.Print("Введите исходную валюту (USD, EUR, RUB): ")
+		_, err := fmt.Scan(&currency)
+		if err != nil {
+			fmt.Println("Такой валюты нет. Введите еще раз.")
+			continue
+		}
+
+		if currency == "USD" || currency == "EUR" || currency == "RUB" {
+			return currency
+		}
+
+		fmt.Println("Такой валюты нет. Введите еще раз.")
+	}
+}
+
+func inputAmountCurrency() float64 {
+	var amount float64
+
+	for {
+		fmt.Print("Введите количество валюты: ")
+		_, err := fmt.Scan(&amount)
+		if err != nil {
+			fmt.Println("Ошибка. Введите заново.")
+			continue
+		}
+
+		return amount
+	}
+}
+
+func inputEndCurrency(beginCurrency string) string {
+	var currency string
+	var message string
+
+	switch beginCurrency {
+	case "USD":
+		message = "Введите целевую валюту (EUR, RUB): "
+	case "EUR":
+		message = "Введите целевую валюту (USD, RUB): "
+	case "RUB":
+		message = "Введите целевую валюту (USD, EUR): "
+	}
+
+	for {
+		fmt.Print(message)
+		_, err := fmt.Scan(&currency)
+		if err != nil {
+			fmt.Println("Такой валюты нет. Введите еще раз.")
+			continue
+		}
+
+		if currency != beginCurrency || currency == "USD" || currency == "EUR" || currency == "RUB" {
+			return currency
+		}
+
+		fmt.Println("Такой валюты нет. Введите еще раз.")
+	}
 }
